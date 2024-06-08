1a54286bab8ffc0996bb5fb6db6ea10245d122a4
shebll <shebblloll@gmail.com>
2024-06-09 01:19:40 +0300

    fix useRouter issue

--- src/components/notifications/helper/Notification.tsx
+++ src/components/notifications/helper/Notification.tsx
@@ -1,20 +1,21 @@
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Notification = ({ notification }: any) => {
-  const navigate = useNavigate();
-  const notificationDate = calculateDuration(notification.dateCreated);
+  const router = useRouter();
+  // const notificationDate = calculateDuration(notification.dateCreated);
 
   return (
     <div
       key={notification.id}
       className="notification-container"
-      onClick={() => navigate(`/forums/${notification.resources.postId}`)}
+      onClick={() => router.push(`/forums/${notification.resources.postId}`)}
     >
       <div className="notification-message">
         <p>{notification.message}</p>
       </div>
       <div className="notification-info">
-        <p>{notificationDate}</p>
+        <p>{notification.dateCreated}</p>
         <p>{notification.type}</p>
       </div>
     </div>
