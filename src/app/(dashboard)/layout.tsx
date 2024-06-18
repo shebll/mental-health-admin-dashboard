78096d7336652bbe4dd63b55bc6bed842b5af98e
shebll <shebblloll@gmail.com>
2024-06-18 13:37:25 +0300

    fix notifications error

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,15 +9,12 @@ export default function AdminLayout({
   const { token, user } = useAuth();
   if (token && user) {
     return (
-      // <NotificationsProvider token={token} userId={user.id}>
       <div className="flex h-screen">
         <Sidebar />
         <div className="flex-1 flex flex-col gap-4 h-screen overflow-auto">
-          {/* <NavBar /> */}
           <div className="p-4 md:p-6 lg:p-10 2xl:p-20 ">{children}</div>
         </div>
       </div>
-      // </NotificationsProvider>
     );
   }
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import NotificationIcon from "./NotificationIcon";
 
 export function NavBar() {
-  return (
-    <div className="w-full sticky top-0 px-6 py-4 bg-secondary">
-      <NotificationIcon />
-    </div>
-  );
+  return <div className="w-full sticky top-0 px-6 py-4 bg-secondary"></div>;
 }
